8b98ee1af45c3cee633e9f9fa13b6a715e326ddc
joohyelim1208 <jhl01208@gmail.com>
2025-12-18 16:46:55 -0800

    faet: 상단 이미지 UI구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
     return MaterialApp(home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-  
-}
-
-class _HomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  ///### body
-///- SingleChildScrollView => 자녀 위젯이 많으면 사용 안한다!!
-///- ListView
-///   - ClipRRext : 자녀위젯을 그냥 잘라준다. borderRadius속성이 있음 (많이씀! Container는 안됨)
-///      - Image
-///   - Row (제목행 이라고 부르자)(TODO로 간격 구현하면서 고민!)
-///      - Text
-///      - Text(>도 아이콘이 아니라 그냥 글자로 쳐줬다)
-///     (글자 사이를 띄워주는 방법은 3가지가 있다.)
-///   - Row
-///     - Column (큰앨범 이라고 부르자. 같은 구조 3개)
-///       - ClipRRect
-///          - Image
-///       - Text
-///       - Text
-///    - 큰앨범
-///    - 큰앨범
-///  - 제목행
-///  - Row (작은앨범 이라고 부르자.)
-///     - ClipRRect
-///        - Image
-///     - Column
-///        - Text
-///        - Text
-///     - Icon 
-///  - 작은앨범
-///  - 작은앨범
+/// - ListView
+///     - ClipRRect : 자녀위젯 그냥 잘라줌
+///         - Image
+///     - Row (제목행 이라고 부르자!)(TODO 간격 구현하면서 고민!)
+///         - Text
+///         - Text
+///     - Row
+///         - Column (큰앨범 이라고 부르자!)
+///             - ClipRRect
+///                 - Image
+///             - Text
+///             - Text
+///         - 큰앨범
+///         - 큰앨범
+///     - 제목행
+///     - Row (작은앨범 이라고 부르자!)
+///         - ClipRRect
+///             - Image
+///         - Column
+///             - Text
+///             - Text
+///         - Icon
+///     - 작은앨범
+///     - 작은앨범
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: homeAppBar(),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          // 사이즈드 박스가 더 보기 좋다 세로방향이니까
+          SizedBox(height: 20),
+          // 상단 이미지
+          // AspectRatio 부모위젯에 넣어주면 container에 적용됨
+          // ! 반응형으로 만들지 않으면 사이즈가 작아지면 위젯 안의 다른 텍스트같은게 화면이 잘려서 안 보일수도
+          AspectRatio(
+            // 반드시 넣어줘야 한다.
+            // 가로크기 / 세로크기
+            aspectRatio: 2.5 / 1,
+            child: ClipRRect(
+              // 셀렉트 위젯 해서 AspectRatio
+              borderRadius: BorderRadius.circular(20), // 숫자는 반지름 값이다.
+              child: Image.network(
+                'https://picsum.photos/300/200',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // 제목행
+          // 큰앨범들어가는 로우
+          // 제목행
+          // 작은앨범들어가는 로우
+        ],
+      ),
+    );
+  }
+
+  AppBar homeAppBar() {
+    return AppBar(
+      actions: [
+        Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.black,
+            //
+          ),
+          child: Icon(Icons.search, color: Colors.white),
+        ),
+        SizedBox(width: 16),
+      ],
+      // title 속성은 안드로이드에서는 왼쪽 기본
+      // 아이폰에서는 가운데 기본
+      // centerTitle false 주게되면은 왼쪽으로 가지만
+      // title에 Column 을 배치하면 Column 크기가 앱바 영역전체를
+      // 차지하기 때문에 무용지물
+      // 이럴땐 Column의 crossAxisAlignment 속성으로 조정
+      // Column과 Row의 crossAxisAlignment 기본값은 가운데!
       title: Column(
-        // 
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
@@ -80,37 +100,35 @@ class HomePage extends StatelessWidget {
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
+              //
             ),
           ),
           Row(
             children: [
               Container(
-                width: 50,
-                height: 50,
+                width: 16,
+                height: 5,
                 decoration: BoxDecoration(
                   color: Colors.black,
                   borderRadius: BorderRadius.circular(100),
-                  ),
+                  //
+                ),
               ),
-              SizedBox(width: 16),
+              SizedBox(width: 3),
               Container(
                 width: 5,
                 height: 5,
                 decoration: BoxDecoration(
                   color: Colors.grey,
-                  borderRadius: BorderRadius.circular(100)
-                  ),
+                  borderRadius: BorderRadius.circular(100),
+                  //
+                ),
               ),
-          )
+              //
+            ],
+          ),
         ],
       ),
-      ),
-      //
     );
   }
-
 }
-}
-
-// 코드를 짜다가 중간에 많이 꼬여버렸다..
-// 구조를 잡는 개념만 이해하고 개인과제로 넘어가기로 했다.
