fe0d845f9022a55a2972debbccd21ab407ce5cb1
joohyelim1208 <jhl01208@gmail.com>
2025-12-21 16:52:07 -0800

    feat: 큰앨범 완성

--- lib/rounded_image.dart
+++ lib/rounded_image.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class RoundedImage extends StatelessWidget {
   const RoundedImage({super.key, required this.imageUrl});
 
+  final String imageUrl;
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       // 셀렉트 위젯 해서 AspectRatio
       borderRadius: BorderRadius.circular(20), // 숫자는 반지름 값이다.
-      child: Image.network(ImageUrl, fit: BoxFit.cover),
+      child: Image.network(imageUrl, fit: BoxFit.cover),
     );
   }
 }
