5880e2c858fd6f6db0cff7255ca4632f3fda42cb
joohyelim1208 <jhl01208@gmail.com>
2025-12-23 16:27:45 -0800

    feat: 빈페이지

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('블로그 샘플'),
+        //
+      ),
+    );
+  }
+}
