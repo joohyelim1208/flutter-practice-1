7bd2a41f71cf06d3cfa27f22a66bf5293563598f
joohyelim1208 <jhl01208@gmail.com>
2025-12-29 16:27:14 -0800

    feat: UI 완성

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("오늘의 날씨")),
+      // 글자와 아이콘이 화면 정 가운데에 위치함
       body: SizedBox(
         width: double.infinity,
         child: Column(
